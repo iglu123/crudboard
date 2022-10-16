c39df119fc382550d2ec3b9a0f79900654339f4c
Alvin <iglu123@naver.com>
2022-10-17 04:57:50 +0900

    10.17 nav

--- src/main/java/com/example/makeboard/QuestionRepository.java
+++ src/main/java/com/example/makeboard/QuestionRepository.java
@@ -4,8 +4,9 @@ import com.example.makeboard.Domain.Question.question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 public interface QuestionRepository extends JpaRepository <question, Integer > {
-    Page<question> findAllBy(Pageable pageable);
-
+    Page<question> findAll(Pageable pageable);
+    Page<question> findAll(Specification<question> spec, Pageable pageable);
 }
